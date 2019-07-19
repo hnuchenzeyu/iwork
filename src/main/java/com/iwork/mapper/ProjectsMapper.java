4c9588ed60aa61d52e5595afe4e1227ad0ea56df
XIAO1e <2182772477@qq.com>
2019-07-19 11:30:42 +0800

    项目模块功能

---
+++ src/main/java/com/iwork/mapper/ProjectsMapper.java
@@ -0,0 +1,9 @@
+package com.iwork.mapper;
+
+import com.iwork.bean.Project;
+
+import java.util.List;
+
+public interface ProjectsMapper {
+    List<Project> showProjects();
+}
---
+++ src/main/java/com/iwork/service/ActivityServiceImpl.java
@@ -0,0 +1,30 @@
+package com.iwork.service;
+
+import com.iwork.bean.Activity;
+import com.iwork.mapper.ActivityMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class ActivityServiceImpl implements ActivityService {
+
+    @Resource
+    ActivityMapper mapper;
+
+    public void ActivityAdd(Activity activity) {
+        mapper.addActivity(activity);
+    }
+
+    public List<Activity> ActivityList(int id) {
+        return mapper.listActivities(id);
+    }
+
+
+    public Activity selectActivityById(String id) {
+        return mapper.selectActivity(id);
+    }
+
+
+}
